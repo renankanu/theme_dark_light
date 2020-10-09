e003e97ce5f3b825eb11511a90c069a937dc6c4d
renankanu <renankanu@gmail.com>
2020-10-08 22:58:15 -0300

    Feature - slidy generate

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:thme_dark_light/app/screens/home_screen.dart';
+import 'package:thme_dark_light/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: HomeScreen());
-  }
-}
+void main() => runApp(ModularApp(module: AppModule()));
